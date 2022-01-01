b36025f4c917d960c6c67428c0bec6529953f181
bobbytables2021 <96928228+bobbytables2021@users.noreply.github.com>
2021-12-31 18:21:18 -0600

    Create start.js

---
+++ start.js
@@ -0,0 +1,17 @@
+// This is the starting script that bootstraps
+// the init script on n00dles and then preps
+// foodnstuff for running all of the management
+// scripts.
+// These two were chosen because they don't need
+// any ports opened and start with the right
+// amount of ram.
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	ns.nuke("n00dles");
+	await ns.scp("init.js", "home", "n00dles");
+	ns.nuke("foodnstuff");
+	const scripts = ns.ls("home", ".js");
+	await ns.scp(scripts, "home", "foodnstuff");
+	ns.exec("init.js", "n00dles");
+}
