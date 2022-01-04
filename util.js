938f762262b704635e80422b2193b6cdddb81952
bobbytables2021 <96928228+bobbytables2021@users.noreply.github.com>
2022-01-04 11:28:11 -0600

    Update util.js

--- util.js
+++ util.js
@@ -6,3 +6,38 @@
 export function GetHosts(ns) {
 	return JSON.parse(ns.read("/tmp/hosts.txt"));
 }
+
+/** @param {NS} ns **/
+export function FindHost(hosts, name) {
+	for (var i = 0; i < hosts.length; i++) {
+		if (hosts[i].hostname == name) {
+			return hosts[i];
+		}
+	}
+	return null;
+}
+
+/** @param {NS} ns **/
+export function GetPath(ns, destination) {
+	const hosts = GetHosts(ns);
+	var current = FindHost(hosts, destination);
+	if (current == null) {
+		return [];
+	}
+	var path = [current.hostname];
+	while (current.parent != "home") {
+		current = FindHost(hosts, current.parent);
+		path.push(current.hostname);
+	}
+	path.reverse();
+	return path;
+}
+
+/** @param {NS} ns **/
+export function GetReserveCash(ns) {
+	const content = ns.read("/tmp/reserve-money.txt");
+	if (content == "") {
+		return 0;
+	}
+	return JSON.parse(content);
+}
